b3122823ae767548f89ee633f7dce40397051126
Cody Lieu <cody.a.lieu@gmail.com>
2014-10-10 19:57:14 -0400

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dukeTextbookMarketplaceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dukeTextbookMarketplaceApp
+ */
+angular.module('dukeTextbookMarketplaceApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
