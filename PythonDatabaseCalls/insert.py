c0e0f4fea5beaa56b6ecd8f2887e32c50cad5f21
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-12-06 16:49:52 -0500

    fix insert url issue

--- PythonDatabaseCalls/insert.py
+++ PythonDatabaseCalls/insert.py
@@ -3,6 +3,7 @@ from urllib2 import Request, urlopen, URLError
 
 def insertToClassToBook(class_id , isbn):
     url = "http://colab-sbx-211.oit.duke.edu/DukeTextbookMarketplace/PHPDatabaseCalls/class_to_book/insert.php?class_id='"+class_id+"'&isbn='"+isbn+"'"
+    url = fixURL(url)
     req = Request(url)
     try:
         response = urlopen(req)
@@ -11,6 +12,7 @@ def insertToClassToBook(class_id , isbn):
         
 def insertToCourse(class_id, dept_id, course_num, course_name):
     url = "http://colab-sbx-211.oit.duke.edu/DukeTextbookMarketplace/PHPDatabaseCalls/course/insert.php?class_id='"+class_id+"'&dept_id='"+dept_id+"'&course_num='"+course_num+"'&course_name='"+course_name+"'"
+    url = fixURL(url)
     req = Request(url)
     try:
         response = urlopen(req)
@@ -19,6 +21,7 @@ def insertToCourse(class_id, dept_id, course_num, course_name):
         
 def insertToDepartment(dept_id, deptName):
     url = "http://colab-sbx-211.oit.duke.edu/DukeTextbookMarketplace/PHPDatabaseCalls/department/insert.php?dept_id='"+dept_id+"'&deptName='"+deptName+"'"
+    url = fixURL(url)
     req = Request(url)
     try:
         response = urlopen(req)
@@ -27,6 +30,7 @@ def insertToDepartment(dept_id, deptName):
         
 def insertToListings(listing_id, netid, isbn, statusOfBook, conditionOfBook, price):
     url = "http://colab-sbx-211.oit.duke.edu/DukeTextbookMarketplace/PHPDatabaseCalls/listings/insert.php?listing_id='"+listing_id+"'&netid='"+netid+"'&isbn='"+isbn+"'&statusOfBook='"+statusOfBook+"'&conditionOfBook='"+conditionOfBook+"'&price='"+price+"'"
+    url = fixURL(url)
     req = Request(url)
     try:
         response = urlopen(req)
@@ -35,7 +39,7 @@ def insertToListings(listing_id, netid, isbn, statusOfBook, conditionOfBook, pri
 
 def insertToScore(score):
     url = "http://colab-sbx-211.oit.duke.edu/DukeTextbookMarketplace/PHPDatabaseCalls/score/insert.php?score='"+score+"'"
-    req = Request(url)
+    url = fixURL(url)
     req = Request(url)
     try:
         response = urlopen(req)
@@ -45,15 +49,18 @@ def insertToScore(score):
     
 def insertToTextbooks(isbn, title, author, description, edition):
     url = "http://colab-sbx-211.oit.duke.edu/DukeTextbookMarketplace/PHPDatabaseCalls/textbooks/insert.php?isbn='"+isbn+"'&title='"+title+"'&author='"+author+"'&description='"+description+"'&edition='"+edition+"'"
+    url = fixURL(url)
     print url
     req = Request(url)
     try:
         response = urlopen(req)
+        print response.read()
     except URLError,e:
         print "Got error code: ", e
         
 def insertToTransactions(netid, listing_id, type, status, date_paid):
     url = "http://colab-sbx-211.oit.duke.edu/DukeTextbookMarketplace/PHPDatabaseCalls/transactions/insert.php?netid='"+netid+"'&listing_id='"+listing_id+"'&type='"+type+"'&status='"+status+"'&date_paid='"+date_paid+"'"
+    url = fixURL(url)
     req = Request(url)
     try:
         response = urlopen(req)
@@ -62,11 +69,16 @@ def insertToTransactions(netid, listing_id, type, status, date_paid):
         
 def insertToUsers(netid, firstName, lastName, major, phoneNumber):
     url = "http://colab-sbx-211.oit.duke.edu/DukeTextbookMarketplace/PHPDatabaseCalls/Users/insert.php?netid='"+netid+"'&firstName='"+firstName+"'&lastName='"+lastName+"'&major='"+major+"'&phoneNumber='"+phoneNumber+"'"
+    url = fixURL(url)
     req = Request(url)
     try:
         response = urlopen(req)
     except URLError,e:
         print "Got error code: ", e
+        
+def fixURL(url):
+    return url.replace (" ", "%20")
+    
     
 if __name__ == '__main__':
 #     insertToUsers("user1", "jack", "fucker", "cs", "121142144214")
