36838df80e74188b9aea56f5b9fa9a777ed31ab1
sdh31 <sdh31@duke.edu>
2014-12-08 09:34:15 -0500

    updates to update

--- PHPDatabaseCalls/department/update.php
+++ PHPDatabaseCalls/department/update.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/javascript');
 header("access-control-allow-origin: *");
 
-$con = mysqli_connect("localhost", "root", "bitnami", "health_alertdb");
+$con = mysqli_connect("localhost", "root", "bitnami", "duke_textbook_marketplace");
 
 if(mysqli_connect_errno()){
   echo "Fail to connect to MySQL: ". mysqli_connect_errno();
